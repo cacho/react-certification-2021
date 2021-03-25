ff25a6f274ad4de73260051e26921efe8de41635
Luis Venegas <luis.venegas@neuronapixel.com>
2021-03-25 01:22:16 -0600

    - Changes on Modal approach

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,12 +1,44 @@
-import React, { useEffect } from 'react';
-import useModal from '../../hooks/useModal';
+import React, { useEffect, useState } from 'react';
+import { Modal } from 'bootstrap';
+import { useHistory } from 'react-router-dom';
+
+import PortalModal from '../../components/PortalModal/PortalModal.component';
+import Login from '../../components/Login/Login.component';
 
 function LoginPage() {
-  const { displayModal, modal } = useModal();
-  // displayModal();
+  const history = useHistory();
+  const goBack = () => {
+    history.goBack();
+  };
+  const [showModal, setShowModal] = useState(true);
+
   useEffect(() => {
-    displayModal();
-  }, []);
+    if (showModal) {
+      const modalElement = document.getElementById('portalModalContainer');
+      const userNameField = document.getElementById('username');
+      const portalModal = new Modal(modalElement, {
+        keyboard: false,
+        focus: true,
+      });
+      modalElement.addEventListener('shown.bs.modal', () => {
+        if (userNameField) {
+          userNameField.focus();
+        }
+      });
+      modalElement.addEventListener('hidden.bs.modal', () => {
+        setShowModal(false);
+        goBack();
+      });
+      portalModal.show();
+    }
+  });
+
+  const modal = (
+    <PortalModal>
+      <Login />
+    </PortalModal>
+  );
+
   return <>{modal}</>;
 }
 
